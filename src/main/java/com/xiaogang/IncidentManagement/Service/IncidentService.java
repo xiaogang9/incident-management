5d83489056e78fc264bf822bc84a9e38e2aa989e
xiaogang lv <161275070+xiaogang9@users.noreply.github.com>
2024-11-02 17:32:43 +0800

    Update IncidentService.java

--- src/main/java/com/xiaogang/IncidentManagement/Service/IncidentService.java
+++ src/main/java/com/xiaogang/IncidentManagement/Service/IncidentService.java
@@ -9,31 +9,59 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
 @Service
 public class IncidentService {
     @Autowired
     private IncidentRepository repository;
 
+    /**
+     * Create incident in the H2 DB
+     * @param incident passed from controller layer
+     * @return incident entity
+     */
     public Incident createIncident(Incident incident) {
         if (repository.existsByTitle(incident.getTitle())) {
             throw new IllegalArgumentException("Incident with this title already exist.");
         }
+        incident.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        incident.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
         return repository.save(incident);
     }
 
+    /**
+     * Return a list of incidents
+     * @param pageable pagination controlled by each page displays 20 incidents
+     * @return pageable incidents
+     */
     @Cacheable("incidents")
     public Page<Incident> getAllIncidents(Pageable pageable) {
         return repository.findAll(pageable);
     }
 
-    public Incident updateIncident(Long id, Incident incidentDetails) {
+    /**
+     * update incident in the service layer
+     * @param id incident id
+     * @param incidentDetail incident entity
+     * @return incident entity
+     */
+    public Incident updateIncident(Long id, Incident incidentDetail) {
         Incident incident = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Incident not found for this id :: " + id));
-        incident.setTitle(incidentDetails.getTitle());
-        incident.setDescription(incidentDetails.getDescription());
+        incident.setTitle(incidentDetail.getTitle());
+        incident.setDescription(incidentDetail.getDescription());
+        incident.setLevel(incidentDetail.getLevel());
+        incident.setStatus(incidentDetail.getStatus());
+        incident.setAssignedTo(incidentDetail.getAssignedTo());
+        incident.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
         return repository.save(incident);
     }
 
+    /**
+     * Delete incident in the service layer
+     * @param id incident id
+     */
     public void deleteIncident(Long id) {
         if (!repository.existsById(id)) {
             throw new ResourceNotFoundException("Incident not found for this id :: " + id);
